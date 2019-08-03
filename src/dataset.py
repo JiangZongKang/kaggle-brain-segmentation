6c2eec6a722dbc7ed6f48ae6c21d5a782ddc3869
JiangZongKang <jzksuccess@gmail.com>
2019-08-03 12:57:25 +0800

    init push code

--- src/dataset.py
+++ src/dataset.py
@@ -12,10 +12,17 @@
 
 import os
 import random
-
 import numpy as np
 import torch
 from skimage.io import imread
 from torch.utils.data import Dataset
-
 from utils import crop_sample, pad_sample, resize_sample, normalize_volume
+
+class BrainSegmentationDataset(Dataset):
+    'Brain MRI dataset for Flair abnormality'
+
+    in_channels = 3
+    out_channels = 1
+
+    def __init__(self, image_dir, transform=None, image_size=256, suset='train', random_sampling=True, validation_cases=10, seed=7):
+        assert subset in ['all', 'train', 'validation']

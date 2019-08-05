739affb7ff07536b07ccf2e5c083c8ab81a21e33
JiangZongKang <jzksuccess@gmail.com>
2019-08-05 09:33:17 +0800

    init push code

--- src/dataset.py
+++ src/dataset.py
@@ -26,3 +26,37 @@ class BrainSegmentationDataset(Dataset):
 
     def __init__(self, image_dir, transform=None, image_size=256, suset='train', random_sampling=True, validation_cases=10, seed=7):
         assert subset in ['all', 'train', 'validation']
+        # read images
+        volumes = {}
+        masks = {}
+        print('reading {} images ...'.format(subset))
+        for (dirpath, dirnames, filenames) in os.walk(images_dir):
+            image_slices = []
+            mask_slices = []
+            for filename in sorted(filter(lambda f: '.tif' in f, filenames),
+                                   key=lambda x: int(x.split('.')[-2].split('_')[4])):
+                filepath = os.path.join(dirpath, filename)
+                if 'mask' in filename:
+                    mask_slices.append(imread(filepath, as_gray=True))
+                else:
+                    image_slices.append(imread(filepath))
+                if len(image_slices) > 0:
+                    patient_id = dirpath.split('/')[-1]
+                    volumes[patient_id] = np.array(image_slices[1:-1])
+                    masks[patient_id] = np.array(mask_slices[1: -1])
+
+                self.patients = sorted(volumes)
+
+                # select cases to subset
+                if not subset == 'all':
+                    random.seed(seed)
+                    validation_patients = random.sample(self.patients, k=validation_cases)
+                    if subset == 'validation':
+                        self.patients = validation_patients
+                    else:
+                        self.patients = sorted(list(set(self.patients).difference(validation_patients)))
+
+                print('preprocessing {} volumes...'.format(subset))
+
+
+
